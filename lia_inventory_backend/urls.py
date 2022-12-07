d35eacbddd8393c7cd9053bed546704a5f889de5
azhar81 <a.hassanuddin02@gmail.com>
2022-12-07 18:21:32 +0700

    added api call to return token

--- lia_inventory_backend/urls.py
+++ lia_inventory_backend/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+from rest_framework.authtoken import views as authtoken_views
 from users import views
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', authtoken_views.obtain_auth_token),
     path('', include('users.urls')),
     path('', include('vendors.urls')),
     path('', include('vendors.urls')),
